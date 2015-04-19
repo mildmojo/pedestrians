21b752cfcfd3ded275ca07c299495c31df7d1d75
mildmojo <theaterfire@yahoo.com>
2015-04-19 18:10:45 -0400

    Adds menu. Needs work.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+  void OnStart() {
+    Application.LoadLevel("game");
+  }
+
+  void OnCredits() {
+    Application.LoadLevel("credits");
+  }
+
+  void OnExit() {
+    Application.Quit();
+  }
+
+  void Update() {
+    if (Input.GetKeyDown(KeyCode.Escape)) {
+      OnExit();
+    }
+  }
+}
